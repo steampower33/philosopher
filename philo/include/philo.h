6e85fbb9879a0c3ef544a3b85111f75f4a4df01c
SeungMinLee <seunlee2@student.42seoul.kr>
2023-11-21 17:49:41 +0900

    init

---
+++ philo/include/philo.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunlee2 <seunlee2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 14:03:37 by seunlee2          #+#    #+#             */
+/*   Updated: 2023/11/21 16:52:13 by seunlee2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+	int	num;
+	int	die;
+	int	eat;
+	int	sleep;
+	int	max_eat;
+}	t_philo;
+
+#endif
